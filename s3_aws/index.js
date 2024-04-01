02a5b44076e34eb887f74d98af78cefc21596283
Prathyarthi <prathyarti@gmail.com>
2024-04-01 23:10:06 +0530

    AWS S3 with presigned url generation code

---
+++ s3_aws/index.js
@@ -0,0 +1,23 @@
+import { GetObjectCommand, S3Client } from "@aws-sdk/client-s3";
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
+import { config } from "dotenv";
+config()
+
+const s3Client = new S3Client({
+    region: "ap-south-1",
+    credentials: {
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+    }
+});
+
+
+async function getObjectCommand() {
+    const command = new GetObjectCommand({
+        Bucket: process.env.AWS_BUCKET_NAME,
+        Key: key
+    })
+
+    const url = await getSignedUrl(s3Client, command)
+    return url
+} 
